a3cbea339cbbf93f70514f815d2abe0fb8067a63
kabir sattyani <148556887+kabirrrrz@users.noreply.github.com>
2024-05-02 11:11:22 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+let speech=new SpeechSynthesisUtterance();
+
+
+
+
+
+document.querySelector("button").addEventListener("click",() =>{speech.text=document.querySelector("textarea").value;
+window.speechSynthesis.speak(speech);
+});
